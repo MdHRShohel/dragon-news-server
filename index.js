087483824d7b2332958c240d4235e3be3b46ae96
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-22 03:55:58 +0600

    category.json added

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World Our News API is working');
+});
+
+app.get('/news-categories',(req,res)=>{
+    res.send(categories);
 });
 
 app.listen(port,()=>{
