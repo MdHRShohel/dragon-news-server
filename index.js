34ce90988a7a43a53acd023afecc81528c6c3004
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-22 05:45:25 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const categories = require('./data/categories.json');
 
