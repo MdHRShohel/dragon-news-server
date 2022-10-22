fef3bb100a443ea9d82d97ee3035616ff4589e7a
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-22 16:14:57 +0600

    category data loading API updated

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello World Our News API is working');
+app.get("/", (req, res) => {
+  res.send("Hello World Our News API is working");
 });
 
-app.get('/news-categories',(req,res)=>{
-    res.send(categories);
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((news) => news._id === id);
+  res.send(selectedNews);
 });
 
-app.listen(port,()=>{
-    console.log(`Dragon News Server is running on port ${port}`);
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } 
+  else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log(`Dragon News Server is running on port ${port}`);
 });
