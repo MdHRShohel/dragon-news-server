cd0dfce6f7e0156c4df225623020dc5e4604b72f
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-24 02:42:48 +0600

    last commit

--- index.js
+++ index.js
@@ -12,21 +12,24 @@ app.get("/", (req, res) => {
   res.send("Hello World Our News API is working");
 });
 
-app.get("/news/:id", (req, res) => {
-  const id = req.params.id;
-  const selectedNews = news.find((news) => news._id === id);
-  res.send(selectedNews);
-});
-
 app.get("/category/:id", (req, res) => {
   const id = req.params.id;
   if (id === "08") {
     res.send(news);
-  } 
-  else {
+  } else {
     const category_news = news.filter((n) => n.category_id === id);
     res.send(category_news);
   }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((news) => news._id === id);
+  res.send(selectedNews);
 });
 
 app.get("/news-categories", (req, res) => {
